1a5a4bd37161b75d4fa9c2ba1972538f1f95f0b4
Chakravarti-Pranjal <chakravartipranjal3@gmail.com>
2024-05-01 16:40:17 +0530

    validation using zod

--- server/router/auth.router.js
+++ server/router/auth.router.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
+const validate = require('../middlewares/validate-middleware');
+const signupSchema = require('../validators/auth.validate');
 
 
 router.route('/').get(authController.home);
 
-router.route('/register').post(authController.register);
+router.route('/register').post( validate(signupSchema), authController.register);
 
 router.route('/login').post(authController.login);
 
