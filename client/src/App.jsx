5d951f5d9bb2eba65866ceafc713d32f6809b553
Chakravarti-Pranjal <chakravartipranjal3@gmail.com>
2024-05-02 06:56:05 +0530

    routes created using react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Home} from './Pages/Home';
+import {About} from './Pages/About';
+import {Contact} from './Pages/Contact';
+import {Services} from './Pages/Services';
+import {Register} from './Pages/Register';
+import {Login} from './Pages/Login';
+
+
 const App = () => {
   return (
-    <div className="App">
-     mern Admin by pranjal
-    </div>
+    <>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
